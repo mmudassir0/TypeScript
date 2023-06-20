94650b787db65fac7eb12c2cfe4a7657df15cafb
mmudassir0 <mudassirabbas.ma@gmail.com>
2023-06-20 18:43:49 +0500

    updated

---
+++ dist/Interface.js
@@ -0,0 +1,29 @@
+var menDetails = {
+    name: "mudassir",
+    age: 12,
+};
+console.log(menDetails);
+var mySkills = {
+    name: "ali",
+    age: 12,
+    skill: function () {
+        return "web";
+    },
+};
+var myWork = {
+    name: "ahmad",
+    age: 13,
+    email: "mudassir@gmail.com",
+    skill: function () { return "web"; },
+};
+var Developer = {
+    name: "",
+    age: 12,
+    email: "",
+    password: "dhs",
+};
+//interface is only in object
+//we can reopen interface
+// we use extends keyword
+//in  type we use intersection
+//# sourceMappingURL=Interface.js.map
